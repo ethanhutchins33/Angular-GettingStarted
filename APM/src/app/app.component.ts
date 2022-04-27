1216551672e7d6234cfc8e214431fda52c2bf6aa
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-04-27 17:04:06 +0100

    Templates, Interpolation, Directives, Data Binding & Pipes

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductListComponent } from "./products/product-list.component";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //any valid html
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  //Our custom project directives, modules and pipes are defined in declarations
+  declarations: [ 
+    AppComponent,
+    ProductListComponent
   ],
+  //3rd party directives, modules and pipes are defined here in Imports
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
