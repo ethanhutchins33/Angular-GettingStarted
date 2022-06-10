c90a796edf0f88724ad46a2f78f39777e7b607c6
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-06-10 10:20:10 +0100

    build and implement a custom pipe

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   //Our custom project directives, modules and pipes are defined in declarations
   declarations: [ 
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   //3rd party directives, modules and pipes are defined here in Imports
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   bootstrap: [
     AppComponent
